7695423a6da56ae223da0528cae4103a67cfed2c
Sampa Nayak <sampanayak987@gmail.com>
2026-07-25 08:34:42 +0530

    Add: middle-of-the-linked-list - submission-1

---
+++ Data Structures & Algorithms/middle-of-the-linked-list/submission-1.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode curr = head;
+        int c = 0;
+        while(curr != null)
+        {
+            c++;
+            curr = curr.next;
+        }
+        curr = head;
+        for(int i = 1; i < (c/2); i++)
+        {
+            curr = curr.next;
+        }
+        return curr.next;
+    }
+}
