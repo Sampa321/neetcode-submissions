d66a61b889cf0da1f99f8e487879ef00ce8e439b
Sampa Nayak <sampanayak987@gmail.com>
2026-08-30 14:25:53 +0530

    Add: special-array-with-x-elements-greater-than-or-equal-x - submission-0

---
+++ Data Structures & Algorithms/special-array-with-x-elements-greater-than-or-equal-x/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        int l = 1, r = nums.length;
+        while(l <= r)
+        {
+            int mid = (l+r)/2;
+            int cnt = 0;
+            for(int num : nums)
+            {
+                if(num >= mid) cnt++;
+            }
+            if(cnt == mid) return mid;
+            else if(cnt < mid) r = mid-1;
+            else l = mid+1;
+        }
+        return -1;
+    }
+}
