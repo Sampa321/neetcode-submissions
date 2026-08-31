b3ea2f1418a53e3389a1e5915825507437f4fa1c
Sampa Nayak <sampanayak987@gmail.com>
2026-08-31 11:00:51 +0530

    Add: sort-array-by-increasing-frequency - submission-0

---
+++ Data Structures & Algorithms/sort-array-by-increasing-frequency/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] frequencySort(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int ele : nums)
+        {
+            map.put(ele, map.getOrDefault(ele, 0)+1);
+        }
+        Integer []arr = new Integer[nums.length];
+        for(int i = 0; i < nums.length; i++)
+        {
+            arr[i] = nums[i];
+        }
+        Arrays.sort(arr, (a,b) ->{
+            if(!map.get(a).equals(map.get(b)))
+            {
+                return map.get(a) - map.get(b);
+            }
+            return b - a;
+        });
+        for(int i = 0; i < arr.length; i++)
+        {
+            nums[i] = arr[i];
+        }
+        return nums;
+    }
+}
